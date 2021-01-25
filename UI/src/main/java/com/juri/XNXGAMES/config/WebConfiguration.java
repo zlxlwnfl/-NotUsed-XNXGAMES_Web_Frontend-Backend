32804c2ad5ba5eba305ccd28217b6004e3b4dae5
juring <zlxlwnfld@gmail.com>
2021-01-25 20:18:20 +0900

    db 설정 변경

--- UI/src/main/java/com/juri/XNXGAMES/config/WebConfiguration.java
+++ UI/src/main/java/com/juri/XNXGAMES/config/WebConfiguration.java
@@ -1,8 +1,12 @@
 package com.juri.XNXGAMES.config;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -33,8 +37,28 @@ public class WebConfiguration implements WebMvcConfigurer,
 	
 	@Override
     public void customize(TomcatServletWebServerFactory factory) {
-        factory.addConnectorCustomizers((TomcatConnectorCustomizer)
-                connector -> connector.setAttribute("relaxedQueryChars", "<>[\\]^`{|}"));
+		factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+			@Override
+			public void customize(Connector connector) {
+				connector.setAttribute("relaxedQueryChars", "<>[\\]^`{|}");
+				connector.setParseBodyMethods("POST,PUT,DELETE");
+			}
+		});
+//        factory.addConnectorCustomizers((TomcatConnectorCustomizer)
+//                connector -> connector.setAttribute("relaxedQueryChars", "<>[\\]^`{|}"));
+//        factory.addConnectorCustomizers((TomcatConnectorCustomizer)
+//                connector -> connector.setParseBodyMethods("POST,PUT,DELETE"));
     }
 	
+//	@Bean
+//	public ServletWebServerFactory servletContainer() {
+//		return new TomcatServletWebServerFactory() {
+//			@Override
+//			protected void customizeConnector(Connector connector) {
+//	               super.customizeConnector(connector);
+//	               connector.setParseBodyMethods("POST,PUT,DELETE");
+//			}
+//		};
+//	}
+
 }
