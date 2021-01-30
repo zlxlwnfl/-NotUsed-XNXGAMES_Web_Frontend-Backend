64d6016895370b7fcf0b8199bf1ddea226f9da6c
juring <zlxlwnfld@gmail.com>
2021-01-30 16:41:44 +0900

    Resilience4j 설정

--- Board/src/main/java/com/juri/XNXGAMES/controller/EventDispatcher.java
+++ Board/src/main/java/com/juri/XNXGAMES/controller/EventDispatcher.java
@@ -6,20 +6,27 @@ import org.springframework.stereotype.Component;
 
 import com.juri.XNXGAMES.domain.BoardToMemberPostMessage;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 
 @Component
 public class EventDispatcher {
 
 	private RabbitTemplate rabbitTemplate;
-	private String exchange = "BoardExchange";
+	private static final String EXCHANGE = "BoardExchange";
+	private static final String RESILIENCE4J_INSTANCE = "default";
 
 	EventDispatcher(RabbitTemplate rabbitTemplate) {
 		this.rabbitTemplate = rabbitTemplate;
 	}
 	
+	@CircuitBreaker(name = RESILIENCE4J_INSTANCE)
+	@Bulkhead(name = RESILIENCE4J_INSTANCE)
+	@Retry(name = RESILIENCE4J_INSTANCE)
 	public void boardToMemberPostSend(BoardToMemberPostMessage message) {
-		rabbitTemplate.convertAndSend(exchange, "BoardToMember.Post", message);
+		rabbitTemplate.convertAndSend(EXCHANGE, "BoardToMember.Post", message);
 	}
 	
 }
--- UI/src/main/java/com/juri/XNXGAMES/service/RpcService.java
+++ UI/src/main/java/com/juri/XNXGAMES/service/RpcService.java
@@ -10,6 +10,9 @@ import org.springframework.web.client.RestTemplate;
 
 import com.juri.XNXGAMES.DTO.PostGetDTO;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -17,13 +20,16 @@ import lombok.AllArgsConstructor;
 public class RpcService {
 
     private RestTemplate restTemplate;
+    private static final String RESILIENCE4J_INSTANCE = "default";
 
+    @CircuitBreaker(name = RESILIENCE4J_INSTANCE)
+	@Bulkhead(name = RESILIENCE4J_INSTANCE)
+	@Retry(name = RESILIENCE4J_INSTANCE)
     public PostGetDTO getPost(String postId) {
     	ResponseEntity<PostGetDTO> result;
     	
     	result = restTemplate.getForEntity(
     			"http://localhost:8000/public/board/post?postId=" + postId,
-    			null,
     			PostGetDTO.class);
     	
     	return result.getBody();
