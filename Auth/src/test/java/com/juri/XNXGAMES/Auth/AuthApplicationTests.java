4454d19dcbfd05a6bd17dbda4de93c30db27a330
juring <zlxlwnfld@gmail.com>
2021-03-09 16:29:21 +0900

    Auth 서비스 추가

---
+++ Auth/src/test/java/com/juri/XNXGAMES/Auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juri.XNXGAMES.Auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
